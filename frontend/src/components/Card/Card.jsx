2d22d663640fe377c3f518c4f29aa070f79f69c0
Edmilson Gomes <edmilsondmx@gmail.com>
2022-04-09 18:02:44 -0300

    card style update

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -10,18 +10,18 @@ export default ({ user }) => (
         <h2 className="card-title">{user.name}</h2>
         <h4 className="card-subtile mb-2 text-muted">{user.specialty}Develop</h4>
         <div>
-          <p>
+          <p className="skill">
             {user.skills.map((skill) => (
               <span className="item-skill">{skill} </span>
             ))}
           </p>
         </div>
       </div>
-
+      
       <div className="icon-item">
-        <i class="fa-brands fa-whatsapp"></i>
-        <i class="fa-brands fa-medium"></i>
-        <i class="fa-solid fa-calendar-check"></i>
+        <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+        <a href="#"><i class="fa-brands fa-medium"></i></a>
+        <a href="#"><i class="fa-solid fa-calendar-check"></i></a>
       </div>
     </div>
     
