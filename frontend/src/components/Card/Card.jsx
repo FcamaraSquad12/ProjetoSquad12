5fec42279c69126a492a9a5df31eb7d80ee13299
Miguel Fialho <fialhomig@gmail.com>
2022-04-09 10:22:51 -0300

    Adicionando efeito nos cards

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import './Card.css'
+import React from "react";
+import "./Card.css";
 
-export default ({user}) => 
-    <div className="card">
-        <div className="card-img"></div>
-        <h1>{user.name}</h1>
-        <div>
-            {
-                user.skills.map((skill) => 
-                    <p>{skill}</p>
-                )
-            }
-            <button>Conectar</button>
-        </div>
+export default ({ user }) => (
+  <div className="card fade-in-fwd">
+    <div className="card-img"></div>
+    <h1>{user.name}</h1>
+    <div>
+      {user.skills.map((skill) => (
+        <p>{skill}</p>
+      ))}
+      <button>Conectar</button>
     </div>
+  </div>
+);
