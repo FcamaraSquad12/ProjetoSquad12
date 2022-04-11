816ae2ca3f900cb44b4387df9f9d1ccbc8dc3fea
Wellington Henrique <wellingtonhlc@gmail.com>
2022-04-11 18:33:36 -0300

    Refatoring person collection

--- backend/models/Person.js
+++ backend/models/Person.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose')
 
 const Person = mongoose.model('Person', {
   name: String,
+  profession: String,
+  description: String,
   email: String,
   password: String,
   skills: Array,
-  interests: Array
+  whatsapp: String,
+  portfolio: String,
+  linkedin: String,
+  drive: String
 })
 
 module.exports = Person
